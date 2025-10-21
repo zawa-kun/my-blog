acc82f240708994c53dabb6ac987994214c3e1df
zawa-kun <shult.0219@gmail.com>
2025-10-22 01:22:27 +0900

    fix:ブログ記事一覧を新しい順に並び替え
    メタデータ内のdateをタイムスタンプに型変換し、比較。
    
    現時点では.mdファイルのメタデータにyyyy-mm-ddの形でなくては正常に動作しない。

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,12 +1,18 @@
 import AllBlogCards from "@/features/blog/allBlogCards";
-import { getAllPostMeta } from "@/lib/meta";
+import { getAllPostMeta, comparePostsByDate } from "@/lib/meta";
+
 
 export default function BlogPage() {
+  // 投稿のメタデータを取得
   const posts = getAllPostMeta();
-
+  // 投稿を降順（新しい順）で並び替え（元配列を変更しない）
+  // 比較関数（comparePostsByDate)により
+  // メタデータ内の日付(date)を取り出し並び替え。
+  const sortedPosts = [...posts].sort(comparePostsByDate);
+  
   return (
     <div className="px-4 sm:px-6 py-18 w-full max-w-6xl mx-auto">
-      <AllBlogCards posts={posts} />
+      <AllBlogCards posts={sortedPosts} />
     </div>
   );
 }
--- src/lib/meta.ts
+++ src/lib/meta.ts
@@ -5,16 +5,39 @@ import { BlogPostMeta } from "@/types/type";
 
 export function getAllPostMeta() :BlogPostMeta[]{
   const dir = path.join(process.cwd(), "src/content/blog");
-  return fs.readdirSync(dir)
-    .filter((file) => file.endsWith(".md"))
+  return fs.readdirSync(dir) // dir(src/content/blog)内のすべてのファイルを読み込む
+    .filter((file) => file.endsWith(".md")) // 拡張子が.mdのファイルのみ取り出す
     .map((file) => {
+      // UTF-8エンコーディングしてファイルを読み込む
+      // matter ライブラリでMarkdownファイルを解析するには、
+      // 文字列として読み込まなくてはいけないから。
       const raw = fs.readFileSync(path.join(dir, file), "utf-8");
+      // matter()でメタデータの解析を取り出す
       const { data } = matter(raw);
+      
       return {
-        slug: file.replace(/\.md$/, ""),
+        slug: file.replace(/\.md$/, ""), // 拡張子を無くす
         title: data.title,
         date: data.date,
         tags: data.tags,
       };
     });
+}
+
+
+// ブログ記事一覧を投稿順（新しい順,降順）に表示するための比較関数
+export function comparePostsByDate(aMeta: BlogPostMeta, bMeta: BlogPostMeta): number {
+  // Date.parse()により、
+  // yyyy-mm-ddの形を
+  // タイムスタンプ（UTCの1970/01/01/0:00からの経過時間に型変換
+  const aTime = Date.parse(aMeta.date);
+  const bTime = Date.parse(bMeta.date);
+
+  // yyyy-mm-dd形式でない場合の処理.
+  if (isNaN(aTime) && isNaN(bTime)) {
+    console.warn(`dateの形式が誤っている可能性があります: aMeta.date=${aMeta.date}, bMeta.date=${bMeta.date}`);
+    return 0; // 両方とも無効な場合、等しいと見なす
+  }
+
+  return bTime - aTime;
 }
